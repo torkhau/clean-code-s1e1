ba82dbd038c4f3bafa7fe545fb6e95a70b7fe1ee
Aliaksandr Torkhau <torkhau@gmail.com>
2021-04-03 17:36:45 +0300

    refactor: renaming to meaningful names of classes according to rule 3.2

--- app.js
+++ app.js
@@ -8,7 +8,7 @@
 
 // Event handling, user interaction is what starts the code execution.
 
-var taskInput=document.getElementById("new-task");//Add a new task.
+var taskInput=document.getElementById("section-addind___new-task__input");//Add a new task.
 var addButton=document.getElementsByTagName("button")[0];//first button
 var incompleteTaskHolder=document.getElementById("list-incompleted");//ul of #incompleteTasks
 var completedTasksHolder=document.getElementById("list-completed");//completed-tasks
