0b2cbb5f337e2950027b42927618dfcddf9ab45a
Aliaksandr Torkhau <torkhau@gmail.com>
2021-04-04 13:36:00 +0300

    fix: add alt attribute to img tags according rule 1.2 advanced rules

--- app.js
+++ app.js
@@ -45,6 +45,7 @@ var createNewTaskElement=function(taskString){
 
     deleteButton.className="list-item__btn-delete";
     deleteButtonImg.src='./remove.svg';
+    deleteButtonImg.alt='';
     deleteButton.appendChild(deleteButtonImg);
 
 
